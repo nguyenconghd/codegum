7d43db9ebd09a483f2e8cddedb8b949315699183
nguyenconghd <88385850+nguyenconghd@users.noreply.github.com>
2022-01-10 23:47:42 +0700

    Add files via upload

---
+++ baitap3.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth=prompt("enter the width");
+inputHeight=prompt("Enter the Height");
+
+width=parseInt(inputWidth);
+height=parseInt(inputHeight);
+
+let area = width*height;
+document.write("The area is:"+area);
